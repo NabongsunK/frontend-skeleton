42a124cffcdcd2287d2653adefce9aeb7cf9abf2
Kilyong Jeong <uzoolove@gmail.com>
2023-10-11 10:27:50 +0900

    .

--- workspace-ins/board-api-server/app.js
+++ workspace-ins/board-api-server/app.js
@@ -65,6 +65,7 @@ app.use("/api", (req, res, next) => {
   res.status(404).json({ error: { message: "존재하지 않는 API입니다." } });
 });
 
+// React용 fallback 추가
 app.use("/", (req, res, next) => {
   res.sendFile(path.join(__dirname, "..", "board-app", "build", "index.html"));
 });
